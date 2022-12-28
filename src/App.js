9c0e5ecfae9802d540076e9985372832fdf844c0
gamagegit <cgamage92@yahoo.com>
2022-12-27 22:57:41 -0800

    crash course started

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Typography from "@mui/material/Typography";
+function App() {
+  return (
+    <div className="app">
+      <Typography variant="h4">Hello There!</Typography>
+    </div>
+  );
+}
+
+export default App;
